b5d32fa916e34291e909f60b6ee2e6e7bf39f7ef
Martina Emmert <emmert.tin1306@gmail.com>
2021-11-08 21:23:55 +0100

    Added comments for solution

--- src/Pyramide/Pyramide.java
+++ src/Pyramide/Pyramide.java
@@ -48,6 +48,11 @@ public class Pyramide extends GraphicsApp {
         drawBackground(BACKGROUND_COLOR);
     }
 
+    /*
+     * Für die Pyramide wird zunächst die x-Position des Steins unten links ausgehend von der Fenstermitte berechnet.
+     * Dann wird für in einer for-Schleife jede Reihe der Pyramide gezeichnet.
+     * Dafür werden x- und y-Position der Reihe sowie die Anzahl der Steine der Reihe berechnet.
+     */
     private void drawPyramid() {
         int xPosStart = HORIZONTAL_CENTER - (BRICK_WIDTH * BRICKS_IN_BASE / 2)  ;
         for (int currentRow = 1; currentRow <= PYRAMID_ROWS; currentRow++) {
@@ -58,6 +63,12 @@ public class Pyramide extends GraphicsApp {
         }
     }
 
+    /*
+     * Diese Methode beschreibt das Zeichnen einer einzelnen Reihe der Pyramide.
+     * Für jede Reihe wird die Anzahl der Steine in der Reihe und die x- und y-Position des ersten Steins übergeben.
+     * In der for-Schleife werden die x-Positionen für die übergebene Anzahl an Steinen berechnet.
+     * Die berechnete x-Position und die übergebene y-Position werden als Parameter an drawBrick() übergeben.
+     */
     private void drawPyramidRow(int numBricks, int xPosStart, int yPos) {
         for (int j = 0; j < numBricks; j++) {
             int xPosBrickInRow = xPosStart + (j * BRICK_WIDTH);
@@ -65,16 +76,29 @@ public class Pyramide extends GraphicsApp {
         }
     }
 
+    /*
+     * In dieser Methode wird ein Stein erstellt und gezeichnet.
+     * Es werden die x- und y-Position des Steins übergeben.
+     * Für eine kleine Lücke zwischen den Steinen wird ein Border der Breite 1 gesetzt.
+     */
     private void drawBrick(int xPos, int yPos){
         Rectangle brick = new Rectangle(xPos, yPos, BRICK_WIDTH, BRICK_HEIGHT, BRICK_COLOR);
         brick.setBorder(BACKGROUND_COLOR, 1);
         brick.draw();
     }
 
+    /*
+     * Die y-Positon der übergebenen Reihe wird ausgehend
+     * von dem unteren Fensterrand und der Höhe eines Steins berechnet.
+     */
     private int calcCurrentYPos(int row){
         return CANVAS_HEIGHT - row * BRICK_HEIGHT;
     }
 
+    /*
+     * Die x-Positon des linken Steins in der übergebenen Reihe wird ausgehend
+     * von der Startposition des unteren linken Steins und der Breite eines Steins berechnet.
+     */
     private int calcCurrentXPos(int row, int xPosStart){
         return xPosStart + ((row -1) * (BRICK_WIDTH / 2));
     }
